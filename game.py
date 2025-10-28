86c0236ca9e5a6f6d3d27aae06c677920bf5870b
asaf_ashwal <ashwalasafx@gmail.com>
2025-10-28 14:59:01 +0200

    commiting befor checkout

--- game.py
+++ game.py
@@ -1,5 +1,12 @@
 from user_func import get_game, user_choic
+from placement import won
 
 def init_game():
     game = get_game()
-    user_choic(game)
+    while True:
+        user_choic(game)
+        if won():
+            print('won')
+            break
+            print('out of ammo')
+        elif game['shots'] == 0:
